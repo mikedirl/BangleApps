b8a5585b0e7894da3d201268b5111b0e0e393a5b
mikedirl <mikedirl@gmail.com>
2023-05-30 00:06:53 +0100

    Update app.js

--- apps/scrollingmessage/app.js
+++ apps/scrollingmessage/app.js
@@ -23,13 +23,13 @@ g.setFontAlign(0, 0);
 g.setColor('#000000');
 g.drawString(g.wrapString(txt, g.getWidth()).join("\n"), g.getWidth()/2, g.getHeight()/2);
 
-Bangle.loadWidgets();
-Bangle.drawWidgets();
+//Bangle.loadWidgets();
+//Bangle.drawWidgets();
 
 //E.showMessage(txt,{
 //    title:"XXL Messages",
 //    img:atob("FBQBAfgAf+Af/4P//D+fx/n+f5/v+f//n//5//+f//n////3//5/n+P//D//wf/4B/4AH4A=") // (i)
 //})
 
-setTimeout(stop, 4000);
+setTimeout(stop, 1);
 
